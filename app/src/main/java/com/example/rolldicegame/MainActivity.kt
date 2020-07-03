24c35b509d279a56100bd19d520dbccec1e93182
sjitprogrammer@gmail.com <sjitprogrammer@gmail.com>
2020-07-03 23:43:44 +0700

    first commit Roll Dice random 1 to 6

---
+++ app/src/main/java/com/example/rolldicegame/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.rolldicegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        roll_button.setOnClickListener { v ->
+            RollDice();
+        }
+    }
+
+    private fun RollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+//        Toast.makeText(applicationContext, "random no. $randomInt", Toast.LENGTH_LONG).show()
+        dice_imageView?.setImageResource(drawableResource)
+    }
+}
